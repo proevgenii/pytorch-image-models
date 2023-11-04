349e78e695238f1c6107435bfe183f6210ad6094
proevgenii <proevgenii19@gmail.com>
2023-11-04 23:16:52 +0800

    update config

--- train.py
+++ train.py
@@ -709,7 +709,8 @@ def main():
         train_loss_fn = nn.CrossEntropyLoss()
     train_loss_fn = train_loss_fn.to(device=device)
     validate_loss_fn = nn.CrossEntropyLoss().to(device=device)
-
+    _logger.info(f'{train_loss_fn}=')
+    _logger.info(f'{validate_loss_fn}=')
     # setup checkpoint saver and eval metric tracking
     eval_metric = args.eval_metric
     best_metric = None
